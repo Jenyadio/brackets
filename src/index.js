00205c628add86f0d16bba687d4f53f8fd84a2de
AppVeyor <ci@appveyor.com>
2023-01-27 14:43:17 +0700

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackets = {};
+  for(let i in bracketsConfig) {
+    brackets[bracketsConfig[i][1]] = bracketsConfig[i].join('');
+  }
+
+  let str2 = str;
+  for(let i = 0; i < str.length; i++) {
+    if(brackets[str[i]] !== undefined) {
+      str2 = str2.replace(brackets[str[i]], '');
+    }
+  }
+
+  return str2.length !== 0 ? false : true;
 }
